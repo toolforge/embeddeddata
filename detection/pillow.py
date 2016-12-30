b93c535d833e3db597647e83dcd78889e7648e7c
zhuyifei1999 <zhuyifei1999@gmail.com>
2016-12-30 15:04:03 +0800

    FileProxy: Split into new utils file & support more seek methods

--- detection/pillow.py
+++ detection/pillow.py
@@ -15,83 +15,16 @@
 # along with self program.  If not, see <http://www.gnu.org/licenses/>
 #
 
+from __future__ import absolute_import
+
 import traceback
 
 from PIL import Image
 from PIL import ImageFile
 
+from detection.utils import FileProxy
+
 ImageFile.MAXBLOCK = 1
-
-
-class FileProxy(object):
-    CHUNK_SIZE = 1 << 16
-
-    def __init__(self, f):
-        self.__f = f
-        self.__pos = self._maxseek = f.tell()
-        self.__chunkpos = None
-        self.__load_chunk()
-
-    def __load_chunk(self):
-        base, ext = divmod(self.__pos, self.CHUNK_SIZE)
-        base *= self.CHUNK_SIZE
-        if self.__chunkpos == base:
-            return
-
-        self.__chunkpos = base
-        # print 'IO'
-        self.__f.seek(base)
-        tell = self.__f.tell()
-        self.chunk = self.__f.read(self.CHUNK_SIZE)
-        self.__pos = tell + min(len(self.chunk), ext)
-
-    def __update(self):
-        self._maxseek = max(self.tell(), self._maxseek)
-
-    def read(self, size=-1):
-        # print 'read(%d)' % size
-        ret = ''
-        if size < 0:
-            while True:
-                ext = self.__pos % self.CHUNK_SIZE
-                r = self.chunk[ext:]
-                self.__pos += len(r)
-                self.__load_chunk()
-                ret += r
-                if not r:
-                    break
-        elif size > 0:
-            ret = ''
-            while True:
-                ext = self.__pos % self.CHUNK_SIZE
-                r = self.chunk[ext:min(len(self.chunk), ext+size)]
-                self.__pos += len(r)
-                size -= len(r)
-                self.__load_chunk()
-                ret += r
-                if not r or not size:
-                    break
-
-        self.__update()
-        return ret
-
-    def readline(self):
-        ret = ''
-        while True:
-            r = self.read(1)
-            ret += r
-            if not r or r == '\n':
-                break
-        return ret
-
-    def seek(self, offset):
-        # print 'seek(%d)' % offset
-        self.__pos = offset
-        self.__load_chunk()
-        self.__update()
-
-    def tell(self):
-        return self.__pos
 
 
 def detect(f):
---
+++ detection/utils.py
@@ -0,0 +1,92 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General License for more details.
+#
+# You should have received a copy of the GNU General License
+# along with self program.  If not, see <http://www.gnu.org/licenses/>
+#
+
+import os
+
+
+class FileProxy(object):
+    CHUNK_SIZE = 1 << 16
+
+    def __init__(self, f):
+        self.__f = f
+        self.__pos = self._maxseek = f.tell()
+        self.__chunkpos = None
+        self.__load_chunk()
+
+    def __load_chunk(self):
+        base, ext = divmod(self.__pos, self.CHUNK_SIZE)
+        base *= self.CHUNK_SIZE
+        if self.__chunkpos == base:
+            return
+
+        self.__chunkpos = base
+        self.__f.seek(base)
+        tell = self.__f.tell()
+        self.chunk = self.__f.read(self.CHUNK_SIZE)
+        self.__pos = tell + min(len(self.chunk), ext)
+
+    def __update(self):
+        self._maxseek = max(self.tell(), self._maxseek)
+
+    def read(self, size=-1):
+        ret = ''
+        if size < 0:
+            while True:
+                ext = self.__pos % self.CHUNK_SIZE
+                r = self.chunk[ext:]
+                self.__pos += len(r)
+                self.__load_chunk()
+                ret += r
+                if not r:
+                    break
+        elif size > 0:
+            ret = ''
+            while True:
+                ext = self.__pos % self.CHUNK_SIZE
+                r = self.chunk[ext:min(len(self.chunk), ext+size)]
+                self.__pos += len(r)
+                print self.__pos
+                size -= len(r)
+                self.__load_chunk()
+                ret += r
+                if not r or not size:
+                    break
+
+        self.__update()
+        return ret
+
+    def readline(self):
+        ret = ''
+        while True:
+            r = self.read(1)
+            ret += r
+            if not r or r == '\n':
+                break
+        return ret
+
+    def seek(self, offset, whence=os.SEEK_SET):
+        if whence == os.SEEK_SET:
+            self.__pos = offset
+        elif whence == os.SEEK_CUR:
+            self.__pos += offset
+        elif whence == os.SEEK_END:
+            raise NotImplementedError  # This breaks the whole detection logic
+        self.__load_chunk()
+        self.__update()
+
+    def tell(self):
+        return self.__pos
