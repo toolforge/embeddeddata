ac60a219d1584be12422ac1d9dd49bbdff73c144
Steinsplitter <steinsplitter-wiki@live.com>
2017-01-19 18:50:16 +0100

    .
    .

--- user-config.py
+++ user-config.py
@@ -4,5 +4,5 @@ family = 'commons'
 mylang = 'commons'
 usernames['commons']['commons'] = u'Embedded Data Bot'
 sysopnames['commons']['commons'] = 'Embedded Data Bot'
-# authenticate['commons.wikimedia.org'] = ('placeholder','placeholder', 'placeholder', 'placeholder')
+# authenticate['commons.wikimedia.org'] = ('placeholder', 'placeholder', 'placeholder', 'placeholder')
 # password_file = '/data/project/yifeibot/.pywikibot/password'
