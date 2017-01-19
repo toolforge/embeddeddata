1a744c637b73d1fbe1c715c8f4569186b9872146
Steinsplitter <steinsplitter-wiki@live.com>
2017-01-19 18:48:24 +0100

    fix
    fix, use oauth

--- user-config.py
+++ user-config.py
@@ -3,4 +3,6 @@
 family = 'commons'
 mylang = 'commons'
 usernames['commons']['commons'] = u'Embedded Data Bot'
+sysopnames['commons']['commons'] = 'Embedded Data Bot'
+# authenticate['commons.wikimedia.org'] = ('placeholder','placeholder', 'placeholder', 'placeholder')
 # password_file = '/data/project/yifeibot/.pywikibot/password'
