4d79a6721d6cd7fdb48d17c6a2349826a664290c
zhuyifei1999 <zhuyifei1999@gmail.com>
2017-04-24 14:52:51 +0800

    user-config: set put_throttle = 1
    No need to add a lot of extra delay between save and delete.

--- user-config.py
+++ user-config.py
@@ -7,3 +7,5 @@ sysopnames['commons']['commons'] = u'Embedded Data Bot'
 
 with open(__import__('os').path.expanduser('~/.oauth-token.json'), 'r') as _f:
     authenticate['commons.wikimedia.org'] = __import__('json').load(_f)
+
+put_throttle = 1
